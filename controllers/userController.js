e3b76085a3ab9dd2b89ff35b093f26a4f903d550
galMiron5 <136713466+galMiron5@users.noreply.github.com>
2023-08-14 18:38:31 +0300

    Validation for registerUser
    Validation for registerUser + Create a new user + save + err
    
    Co-Authored-By: Omri5790 <136712482+Omri5790@users.noreply.github.com>

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,27 @@ const User = require('../models/User');
 
 exports.registerUser = (req, res) => {
   const { name, dateOfBirth } = req.body;
+  console.log(req.body);
+  // Validation: Check if required fields are present
+  if (!name ||!dateOfBirth) {
+    return res.status(400).json({ error: 'Name and Date of Birth are required' });
+  }
+  const _name = /^[A-Z a-z]{2,30}$/;
+  if (!_name.test(name)) {
+    return res.status(400).json({ error: 'Name must be between 2 and 30 characters long' });
+  }
+  if (typeof (dateOfBirth) != typeof(Date())) {
+  return res.status(400).json({ error: 'Date of Birth must be a valid date' });
+  }else{if(dateOfBirth >= Date(today.getFullYear()-12, today.getMonth, today.getDate())){
+    return res.status(400).json({ error: 'User must be 12 years old or older ' });
+  }
+    // Create a new user
+    const newUser = new User();
+    newUser.name = name;
+    newUser.dateOfBirth = dateOfBirth;
+  }
+  newUser.save(err, savedUser);
+};
 
   // Validation: Check if required fields are present
   if (!name || !dateOfBirth) {
