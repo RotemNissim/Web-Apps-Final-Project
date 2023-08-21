fa55514b1057298c040e878b8ebfff6ca405b32b
RotemNissim <rotemnissim28@gmail.com>
2023-08-21 14:37:28 +0300

    fixes

--- controllers/dishController.js
+++ controllers/dishController.js
@@ -3,6 +3,8 @@ const dishesService = require("../services/dishes");
 
 const index = async (req, res) => {
   const dishes = await dishesService.getDishes();
+  console.log(dishes)
+  //const dishes = [{name: 'test', price: 3}]
   res.render("../views/dishes", { dishes });
 };
 
@@ -54,28 +56,28 @@ if(!_restaurant.test(restaurant)) {
 };
 
 const getDish = async (req, res) => {
-  const dish = await restaurantService.getDishById(req.params.id);
+  const dish = await dishesService.getDishById(req.params.id);
   res.json(dish);
 };
 
 const deleteDish = async (req, res) => {
-  const dish = await restaurantService.deleteDish(req.params.id);
+  const dish = await dishesService.deleteDish(req.params.id);
   res.json(dish);
 };
 
 const getDishes = async (req, res) => {
-  const dishes = await restaurantService.getDishes(req.query.pageNum);
+  const dishes = await dishesService.getDishes(req.query.pageNum);
   res.json(dishes);
 };
 
 const getDishesByType = async (req, res) => {
-  const dishes = await restaurantService.getDishesByType(req.params.type);
+  const dishes = await dishesService.getDishesByType(req.params.type);
   res.json(dishes);
 };
 
 const editDish = async (req, res) => {
   const { allergenics, chef, description, name, price, tags, type } = req.body;
-  const dish = await restaurantService.editDish(
+  const dish = await dishesService.editDish(
     req.params.id,
     allergenics,
     chef,
--- server.js
+++ server.js
@@ -1,40 +1,3 @@
-// const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-
-// var app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-// const path = require('path'); // This is the key part you were missing
-
-// const { router: productRouter } = require('./routes/dishes');
-// // Serve static files from the "public" directory
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Websocket connection handling
-// io.on('connection', (socket) => {
-//   console.log('New websocket connection:', socket.id);
-
-//   // Handle incoming websocket events here
-//   // For example, you can emit events to specific clients or broadcast to all connected clients.
-// });
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Start the server
-// const port = process.env.PORT || 8080;
-// server.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-// //routes
-// app.use('/',productRouter);
-// app.use('/about',require('./routes/about'));
-// app.use('/admin',require('./routes/admin'));
-// app.use('/carousel',require('./routes/carousel'));
-// app.use('/login',require('./routes/login'));
-// app.use('suppliers',require('./routes/suppliers'));
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -42,11 +5,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const session = require('express-session');
 
+const { router: dishRouter } = require('./routes/dishes');
+
 const env = process.env.NODE_ENV || "local";
 
 dotenv.config({ path: __dirname + `/config/.env.${env}` });
-
-const { router: dishRouter } = require('./routes/dishes');
 
 mongoose.set('strictQuery',false);
 mongoose.connect(process.env.CONNECTION_STRING, {
@@ -74,7 +37,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
  //routes
- app.use('/',dishRouter);
+ app.use('/', dishRouter);
  app.use('/about',require('./routes/about'));
  app.use('/admin',require('./routes/admin'));
  app.use('/carousel',require('./routes/carousel'));
@@ -83,7 +46,7 @@ app.use(express.json());
 
  const http = require('http').Server(app);
 
- const port = process.env.PORT || 8080;  // Define the port
+ const port = process.env.PORT || 8080;
  
  http.listen(port, () => {
    console.log(`Server is running on port ${port}`);
