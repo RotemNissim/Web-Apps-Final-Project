8829b08997096c75fa974430367668c04f15752b
omerboud <omerboud7712@gmail.com>
2023-08-22 19:34:09 +0300

    log
    log

--- middlewares/logout.js
+++ middlewares/logout.js
@@ -1,15 +1,28 @@
 const logout = (req, res, next) => {
-    req.session.destroy((err) => {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect("/login");
-    });
-  };
-  module.exports = {
-    requireAdminLogin,
-    redirectIfAuthenticated,
-    logout
-  };
+  // Assuming you are using a session-based authentication system
+  // Clear the session data to log the user out
+  req.session.destroy((err) => {
+    if (err) {
+      return next(err);
+    }
+    return res.redirect("/login");
+  });
+};
+
+module.exports = {
+  requireAdminLogin: (pathToRedirect = '/admin') => (req, res, next) => {
+    if (!req.session.adminId) {
+      return res.redirect("/login?pathToRedirect=" + pathToRedirect);
+    }
+    next();
+  },
   
+  redirectIfAuthenticated: (req, res, next) => {
+    if (req.session.adminId) {
+      return res.redirect("/admin");
+    }
+    next();
+  },
   
+  logout
+};
--- routes/logout.js
+++ routes/logout.js
@@ -6,4 +6,8 @@ const middleware = require('./middlewares'); // Adjust the path to your middlewa
 
 router.get('/logout', middleware.logout);
 
+router.get('/some-protected-route', middleware.requireAdminLogin(), (req, res) => {
+  // Handle your protected route logic here
+});
+
 // ...
