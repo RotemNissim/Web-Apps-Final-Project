289f8b08e43d5cea7051373bef7081b1ba3301ec
omerboud <omerboud7712@gmail.com>
2023-08-22 19:50:18 +0300

    Update logout.js

--- routes/logout.js
+++ routes/logout.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const { index, login } = require('../controllers/login');
+const { redirectIfAuthenticated } = require('../middlewares/login');
+
 const router = express.Router();
-const middleware = require('./middlewares'); // Adjust the path to your middleware file
 
-// ...
-
-router.get('/logout', middleware.logout);
-
-router.get('/some-protected-route', middleware.requireAdminLogin(), (req, res) => {
-  // Handle your protected route logic here
+// Logout route definition
+router.get('/logout', (req, res) => {
+  // Clear session data here (if applicable)
+  // Redirect to login page or home page
+  res.redirect('/login'); // Example: Redirect to login page
 });
 
-// ...
+// Index and login routes
+router.route('/').get(redirectIfAuthenticated, index).post(login);
+
+module.exports = router;
