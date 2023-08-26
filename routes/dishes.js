5cd07199e56a740841da510dbf7bfc85b4213a4d
galMiron5 <136713466+galMiron5@users.noreply.github.com>
2023-08-26 19:56:57 +0300

    v

--- routes/dishes.js
+++ routes/dishes.js
@@ -27,7 +27,7 @@ router.route('/api/type/:type').get(getDishesByType);
 router.route('/productPage').get(async (req, res) => {
   try {
     const productId = req.query.id;
-    const dish = await Dish.findById(productId); // Use the correct model name and method
+    const dish = await Dish.findById(productId).populate('restaurant'); // Use the correct model name and method
     res.render('productPage', { product: dish }); // Use the correct variable name
   } catch (error) {
     console.error(error);
